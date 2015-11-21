7bbbee0b3b3a9c6c63e7a28721ed51e8da07784a
Juma314 <tyler.s.schmitz@gmail.com>
2015-11-20 23:20:35 -0500

    retype inputs in AdminSv for further sanitation and handling by AdminEntry

--- SAD_Project4/src/proj4/serverapp/AdminEntry.java
+++ SAD_Project4/src/proj4/serverapp/AdminEntry.java
@@ -1,13 +1,10 @@
 package proj4.serverapp;
-
 import java.sql.Connection;
 import java.util.List;
 import proj4.common.Course;
 
 
-
 public class AdminEntry {
-
 	public Integer enrollLimit;
 	public List<String> semesterCourse;
 	public String semester;
@@ -18,75 +15,116 @@ public class AdminEntry {
 	public ServerApplication myServerApplication;
 
 	public AdminEntry() {
+		
 	}
-
+	
 	public AdminEntry( Connection dbconnection) {
+		
 	}
-
+	
 	public void getEnrollLimit() {
+		
 	}
-
+	
 	/**
-	 * 
-	 * @param limit
-	 */
-	public void setEnrollLimit( int limit, String course) {
+	*
+	* @param limit
+	 * @param error 
+	 * @return 
+	 * @return 
+	*/
+	public String setEnrollLimit( int limit, String course, boolean shadow) {
+		String error = null;
+		
+		return error;
+		
 	}
-
 	public void getSemesterCourse() {
+		
 	}
-
+	
 	/**
-	 * 
-	 */
-	public void addCourse( String desc) {
-		System.out.println("AE: adding course " + desc);
+	 * @param shadow 
+	*
+	*/
+	public String addCourse( String desc, boolean shadow) {
+		String error = null;
+		return error;
 	}
-
+	
+	public String removeCourse( String desc, boolean shadow) {
+		String error = null;
+		return error;
+	}
+	
 	public String getSemester() {
 		return "";
 	}
-
+	
 	public void setSemester( String s) {
+		
 	}
-
+	
 	public void getProfessorById(String id) {
+		
 	}
-
+	
 	public void addProfessor( String profName) {
 		System.out.println("AE: adding professor " + profName);
 	}
-
 	public List<Course> getProfessorCourses(String profId) {
 		return null;
 	}
-
-	public void setProfessorCourses( String profId, List<String> courses) {
-
-	}
-
-	public void getTAPool() {
-	}
-
-	public List<Course> getTACourses(String taId) {
+	
+	public String setProfessorCourses( String profId, String courses, boolean action,
+			boolean mode, boolean shadow) {
+		if(action == true){
+			// add
+		}
+		else
+		{
+			// remove
+		}
+		if(mode == true){
+			// assignment
+		}
+		else
+		{
+			// proficiency
+		}
 		return null;
 	}
-
-	public void invokeShadowMode() {
+	
+	public void getTAPool() {
+		
 	}
-
+	
+	public String setTACourses(String taId, String courses, boolean addOrRemove, 
+			boolean astOrPrfcy, boolean shadow) {
+		
+		
+		return null;
+	}
+	
+	public void invokeShadowMode() {
+		
+	}
+	
 	public void addTA(String name) {
 		System.out.println("AE: adding TA " + name);
 	}
-
+	
 	public void updateTA(String taID, Boolean isAvailNextTerm) {
+		
 	}
-
+	
 	public void updateProf(String profID, Boolean isAvailNextTerm) {
+		
 	}
+	
+	public String getStudentInfo(String name, boolean prefsOrRecs) {
 
-	public void addStudent(String name) {
-		System.out.println("AE: adding student " + name);
+		return null;
 	}
-
+	
 }
--- SAD_Project4/src/proj4/servlets/AdminSv.java
+++ SAD_Project4/src/proj4/servlets/AdminSv.java
@@ -1,10 +1,7 @@
 package proj4.servlets;
-
 import java.io.IOException;
-
 import java.io.PrintWriter;
 import java.util.Map;
-
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -12,103 +9,130 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
-
 import org.apache.tomcat.util.codec.binary.StringUtils;
-
 import proj4.serverapp.AdminEntry;
 import proj4.serverapp.ServerApplication;
 
 /**
- * Servlet implementation class Admin
- */
+* Servlet implementation class Admin
+*/
 @WebServlet("/Admin")
 public class AdminSv extends HttpServlet {
+	
 	private static final long serialVersionUID = 1L;
-    private static AdminEntry ae;
-    
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public AdminSv() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
-    
+	private static AdminEntry ae;
+	
 	/**
-	 * @see Servlet#init(ServletConfig)
-	 */
-	public void init(ServletConfig config) throws ServletException {
-
+	* @see HttpServlet#HttpServlet()
+	*/
+	public AdminSv() {
+		super();
+	// TODO Auto-generated constructor stub
 	}
-
+	
 	/**
-	 * GET requests redirect to the homepage
-	 * 
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	* @see Servlet#init(ServletConfig)
+	*/
+	public void init(ServletConfig config) throws ServletException {
+		
+	}
+	
+	/**
+	* GET requests redirect to the homepage
+	*
+	* @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	*/
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		response.sendRedirect("index.jsp");
 	}
-
+	
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	* @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	*/
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException 
+	{
 		// Get session and check that it is valid and this person is an admin
 		PrintWriter out = response.getWriter();
 		HttpSession session = request.getSession(false);
-		if( session == null || ((String)session.getAttribute("isadmin")).compareTo("1") != 0) {
+		
+		if( session == null || ((String)session.getAttribute("isadmin")).compareTo("1") != 0) 
+		{
 			response.sendRedirect("index.jsp");
 		}
-		if( ae == null ){
+		
+		if( ae == null )
+		{
 			ae = (AdminEntry)((ServerApplication)session.getAttribute("srvapp")).getAdminEntry();
 		}
+		
 		// Set response content type
-	    response.setContentType("text/html");
-
-	    // Determine which form was submitted and call appropriate methods
-	    out.println("Function:  " + request.getParameter("function"));
-	    String f = request.getParameter("function");
-	    if( f.compareTo("setEnrollLims") == 0) {
-	    	String course = request.getParameter("crsname");
-	    	String limit = request.getParameter("crslim");
-	    	String shadow = request.getParameter("modelModeType");
-	    	response.sendRedirect("admin.jsp");
-
-	    } else if( f.compareTo("setCourse") == 0) {
-	    	
-	    	String course = request.getParameter("crsname");
-	    	String mode = request.getParameter("courseSetType");
-	    	String shadow = request.getParameter("modelModeType");
-	    	
-	    	response.sendRedirect("admin.jsp");
-
-	    } else if( f.compareTo("setStaffAsts")== 0){
-	    	
-	    	String courses = request.getParameter("crsname");
-	    	String staffid = request.getParameter("staffname");
-	    	String stafftype = request.getParameter("stafftype");
-	    	String staffUpT1 = request.getParameter("staffUpdateType1");
-	    	String staffUpT2 = request.getParameter("staffUpdateType2");
-	    	String shadow = request.getParameter("modelModeType");
-	    	response.sendRedirect("admin.jsp");
-
-	    } else if(f.compareTo("dispRecsPrefs") == 0){
-	    	String student = request.getParameter("stuname");
-	    	String dispType = request.getParameter("dispType");
-	    	response.sendRedirect("admin.jsp");
-
-	    }
-	    else {
-	    	response.sendRedirect("logout.jsp");
-	    }
-
-	    Map map = request.getParameterMap();
-	    for(int i = 0; i < request.getParameterMap().size(); i++ ){
-	    	
-	    }
-	    
-	    out.println("<script>setTimeout(function () {window.location.href = 'admin.jsp'; }, 2000);</script>");
+		response.setContentType("text/html");
+		// Determine which form was submitted and call appropriate methods
+		out.println("Function: " + request.getParameter("function"));
+		String f = request.getParameter("function");
+		if( f.compareTo("setEnrollLims") == 0) 
+		{
+			String course = request.getParameter("crsname");
+			String limit = request.getParameter("crslim");
+			String shadow = request.getParameter("modelModeType");
+			 
+			String error = ae.setEnrollLimit(Integer.parseInt(limit), course, shadow.equals("shadow"));
+			if(error == null) response.sendRedirect("admin.jsp");
+			else{
+				
+			} //error
+				
+		} else if( f.compareTo("setCourse") == 0) 
+		{
+			String course = request.getParameter("crsname");
+			String mode = request.getParameter("courseSetType");
+			String shadow = request.getParameter("modelModeType");
+			String error = null;
+			if(mode.equals("add")) error =  ae.addCourse(course, shadow.equals("shadow"));
+			else error = ae.removeCourse(course, shadow.equals("shadow"));
+			if(error == null)
+				response.sendRedirect("admin.jsp");
+			else
+			{
+				
+			
+			}
+		} else if( f.compareTo("setStaffAsts")== 0){
+			String courses = request.getParameter("crsname");
+			String staffid = request.getParameter("staffname");
+			String stafftype = request.getParameter("stafftype");
+			String staffUpT1 = request.getParameter("staffUpdateType1");
+			String staffUpT2 = request.getParameter("staffUpdateType2");
+			String shadow = request.getParameter("modelModeType");
+			String error = null;
+			if(stafftype.equals("ta")) error = ae.setTACourses(staffid, courses, 
+					staffUpT2.equals("append"), staffUpT1.equals("assignment"), 
+					shadow.equals("shadow")); 
+			else error = ae.setProfessorCourses(staffid, courses, 
+					staffUpT2.equals("append"), staffUpT1.equals("assignment"), 
+					shadow.equals("shadows"));
+			
+			response.sendRedirect("admin.jsp");
+		} else if(f.compareTo("dispRecsPrefs") == 0){
+			String student = request.getParameter("stuname");
+			String dispType = request.getParameter("dispType");
+			String error = ae.getStudentInfo(student, dispType.equals("prefs") );
+			if (error == null) response.sendRedirect("admin.jsp");
+			else{
+				// err
+			}
+		}
+		else {
+			response.sendRedirect("logout.jsp");
+		}
+		
+		Map map = request.getParameterMap();
+		
+		for(int i = 0; i < request.getParameterMap().size(); i++ ){
+			
+		}
+		
+		out.println("<script>setTimeout(function () {window.location.href = 'admin.jsp'; }, 2000);</script>");
 	}
-
 }
